55b454ccaeb5cd3eb29e7379b08aab4624aa311e
teamwong111 <841713301@qq.com>
2021-02-28 21:24:43 +0800

    fix(loss_metrics):replace np's api with tf's api & reduce duplicate code

--- test/test_loss_metrics/test_loss.py
+++ test/test_loss_metrics/test_loss.py
@@ -6,67 +6,37 @@ from segelectri.loss_metrics.loss import FocalLoss, LovaszLoss, DiceLoss, Bounda
 
 
 class TestLoss(tf.test.TestCase):
+    def setUp(self):
+        self.y_true = tf.random.uniform((2, 512, 512),
+                                        minval=0,
+                                        maxval=3,
+                                        dtype=tf.int64)
+        self.y_pred = tf.random.uniform((2, 512, 512, 3),
+                                        minval=0,
+                                        maxval=1,
+                                        dtype=tf.float32)
+
     def test_focal_loss(self):
-        y_true = tf.random.uniform((2, 512, 512),
-                                   minval=0,
-                                   maxval=3,
-                                   dtype=tf.int64)
-        y_pred = tf.random.uniform((2, 512, 512, 3),
-                                   minval=0,
-                                   maxval=1,
-                                   dtype=tf.float32)
         focall = FocalLoss()
-        loss = focall(y_true, y_pred)
+        loss = focall(self.y_true, self.y_pred)
         self.assertAllEqual(loss.shape, ())
 
     def test_lovasz_loss(self):
-        y_true = tf.random.uniform((2, 512, 512),
-                                   minval=0,
-                                   maxval=3,
-                                   dtype=tf.int64)
-        y_pred = tf.random.uniform((2, 512, 512, 3),
-                                   minval=0,
-                                   maxval=1,
-                                   dtype=tf.float32)
         lovaszl = LovaszLoss()
-        loss = lovaszl(y_true, y_pred)
+        loss = lovaszl(self.y_true, self.y_pred)
         self.assertAllEqual(loss.shape, ())
 
     def test_cross_entropy_loss(self):
-        y_true = tf.random.uniform((2, 512, 512),
-                                   minval=0,
-                                   maxval=3,
-                                   dtype=tf.int64)
-        y_pred = tf.random.uniform((2, 512, 512, 3),
-                                   minval=0,
-                                   maxval=1,
-                                   dtype=tf.float32)
         scce = tf.keras.losses.SparseCategoricalCrossentropy()
-        loss = scce(y_true, y_pred)
+        loss = scce(self.y_true, self.y_pred)
         self.assertAllEqual(loss.shape, ())
 
     def test_dice_loss(self):
-        y_true = tf.random.uniform((2, 512, 512),
-                                   minval=0,
-                                   maxval=3,
-                                   dtype=tf.int64)
-        y_pred = tf.random.uniform((2, 512, 512, 3),
-                                   minval=0,
-                                   maxval=1,
-                                   dtype=tf.float32)
         dicel = DiceLoss()
-        loss = dicel(y_true, y_pred)
+        loss = dicel(self.y_true, self.y_pred)
         self.assertAllEqual(loss.shape, ())
 
     def test_boundary_loss(self):
-        y_true = tf.random.uniform((2, 512, 512),
-                                   minval=0,
-                                   maxval=3,
-                                   dtype=tf.int64)
-        y_pred = tf.random.uniform((2, 512, 512, 3),
-                                   minval=0,
-                                   maxval=1,
-                                   dtype=tf.float32)
         boundaryl = BoundaryLoss()
-        loss = boundaryl(y_true, y_pred)
+        loss = boundaryl(self.y_true, self.y_pred)
         self.assertAllEqual(loss.shape, ())
